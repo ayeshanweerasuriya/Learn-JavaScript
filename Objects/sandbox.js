7c8a9f10e30d84b102efc62660091ebf497eeb37
ayeshanweerasuriya <ayeshanweerasuriya@gmail.com>
2023-12-15 12:52:29 +0530

    Objects

--- Objects/sandbox.js
+++ Objects/sandbox.js
@@ -5,6 +5,18 @@ const ayeshan = {
   sex: "male",
   isAlive: true,
   quotes: ["Good Morning", "Welcome"],
+  callQuotes: function () {
+    console.log(this.quotes);
+  },
+
+  callFinalQoutes: function () {
+    this.quotes.forEach((quote) => {
+      console.log(quote);
+    });
+  },
 };
 
-console.log(ayeshan);
+// let result = ayeshan.quotes[1];
+// ayeshan["name"] = "Kesara";
+
+console.log(ayeshan.callFinalQoutes());
