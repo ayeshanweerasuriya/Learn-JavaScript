c9b8710c6033bdc910e0bf524a013c54ba9f33e3
ayeshanweerasuriya <ayeshanweerasuriya@gmail.com>
2023-12-15 12:38:14 +0530

    Objects

---
+++ Objects/sandbox.js
@@ -0,0 +1,10 @@
+const ayeshan = {
+  name: "Ayeshan",
+  age: 23,
+  address: "wewahena watta road",
+  sex: "male",
+  isAlive: true,
+  quotes: ["Good Morning", "Welcome"],
+};
+
+console.log(ayeshan);
