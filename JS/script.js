753f96510d30ee661ac4813f4088b1544eef6b58
ayeshanweerasuriya <ayeshanweerasuriya@gmail.com>
2023-12-26 14:56:16 +0530

    Exception Handling

--- JS/script.js
+++ JS/script.js
@@ -1,85 +1,45 @@
 const prompt = require("prompt-sync")();
 
-let correctPassword = false;
+const personList = [];
 
-const checkPassword = (password) => {
-  let attempt = 0;
-  while (attempt <= 3) {
-    // Check if the input contains at least one uppercase letter, one digit, and one special character
-    const containsUppercase = /[A-Z]/.test(password);
-    const containsDigit = /\d/.test(password);
-    const containsSpecialCharacter = /[!@#$%^&*()_+{}\[\]:;<>,.?~\\/-]/.test(
-      password
-    );
+const addPerson = (fname, lName, age, gender, isAlive) => {
+  let person = {
+    fName: fname,
+    lName: lName,
+    age: age,
+    gender: gender,
+    isAlive: isAlive,
+  };
 
-    if (
-      password.length < 8 ||
-      !containsDigit ||
-      !containsUppercase ||
-      !containsSpecialCharacter
-    ) {
-      console.log(
-        "Invalid username. It should be at least 8 characters long and contain at least one uppercase letter, one digit, and one special character."
-      );
-      attempt++;
-    } else {
-      correctPassword = true;
-      break;
-    }
-  }
+  personList.push(person);
+  console.log(`${fname} added successfully`);
 };
 
-const logIn = (bringUsername, bringPassword) => {
-  console.log(`Welcome, to login page`);
-  let count = 1;
-  while (count <= 3) {
-    const username = prompt("Username: ");
-    const password = prompt("Password: ");
-    if (username === bringUsername || password === bringPassword) {
-      console.log(`Welcome!, ${username}`);
-      break;
-    } else {
-      console.log(`Username or password incorrect.`);
-      count++;
-      if (count === 3) {
-        while (true) {
-          const wannaSignup = prompt(
-            `Your tries are over. Wanna signup? (press "y" or "n"):`
-          );
-
-          if (wannaSignup.toUpperCase() === "Y") {
-            signUp();
-          }
-
-          if (wannaSignup.toUpperCase() === "N") {
-            console.log("Try again in a moment");
-            break;
-          }
-        }
-      }
-    }
-  }
+const displayperson = () => {
+  personList.forEach((person) => {
+    console.log(`Full name: ${person.fName} ${person.lName}`);
+  });
+  console.log();
 };
 
-const signUp = () => {
-  console.log("Welcome!, Before login you have to create an account");
-  const setUsername = prompt("Set username: ");
-  while (true) {
-    const setPassword = prompt("Set password: ");
+const takeDetails = () => {
+  const fname = prompt("First name: ");
+  const lname = prompt("Last name: ");
+  const age = prompt("Age: ");
+  const gender = prompt("Gender: ");
+  let proNoun;
 
-    if (!setUsername || !setPassword) {
-      console.log("Please enter both username and password.");
-    } else {
-      checkPassword(setPassword);
-      if (correctPassword) {
-        console.log(
-          `-----------------------------------------\nAccount created successfully`
-        );
-        logIn(setUsername, setPassword);
-        break;
-      }
-    }
+  if (gender === "male") {
+    proNoun = "He";
+  } else if (gender === "female") {
+    proNoun = "She";
+  } else {
+    console.log("Gender input Invalid");
   }
+  const isAlive = prompt(`Is ${proNoun} Alive(true or false): `);
+
+  addPerson(fname, lname, age, gender, isAlive);
+  console.log(`------${fname} Added to the list Successfully------`);
 };
 
-// signUp();
+takeDetails();
--- Library Management/sandbox.js
+++ Library Management/sandbox.js
@@ -1,3 +1,4 @@
+const prompt = require("prompt-sync")();
 const library = [];
 
 function addBook(title, author, year) {
@@ -36,11 +37,56 @@ function searchBook(title) {
 }
 
 // Example usage of the functions
-addBook("The Hobbit", "J.R.R. Tolkien", 1937);
-addBook("To Kill a Mockingbird", "Harper Lee", 1960);
-addBook("1984", "George Orwell", 1949);
+// addBook("The Hobbit", "J.R.R. Tolkien", 1937);
+// addBook("To Kill a Mockingbird", "Harper Lee", 1960);
+// addBook("1984", "George Orwell", 1949);
 
-displayBooks();
+// displayBooks();
 
-searchBook("The Hobbit");
-searchBook("Harry Potter");
+// searchBook("The Hobbit");
+// searchBook("Harry Potter");
+
+const menu = () => {
+  while (true) {
+    console.log("<---- Welcome to library management system ---->");
+    console.log(`\n<---- select the option ---->\n`);
+    console.log("1. Add Book");
+    console.log("2. Display Books");
+    console.log("3. Search Book");
+
+    const choice = prompt("Enter your choice (1, 2, or 3): ");
+
+    if (choice == -1) {
+      break;
+    }
+
+    switch (choice) {
+      case "1":
+        takeBook();
+        break;
+      case "2":
+        displayBooks();
+        break;
+      case "3":
+        searchBook();
+        break;
+      default:
+        console.log("Invalid choice. Please enter 1, 2, or 3.");
+    }
+  }
+};
+
+const takeBook = () => {
+  console.log(`\n<--- Add book menu --->\n`);
+  const name = prompt("Book name: ");
+  const author = prompt("Author: ");
+  const year = prompt("Year: ");
+
+  addBook(name, author, year);
+};
+
+const takeSearchBook = () => {
+  const title = prompt("Book title: ");
+  searchBook(title);
+};
+menu();
