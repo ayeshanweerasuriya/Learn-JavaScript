6b1d9b4b516b8a13f69df9bda083096aa90b30e6
ayeshanweerasuriya <ayeshanweerasuriya@gmail.com>
2023-12-28 15:18:37 +0530

    sql

--- sqlConnect-Library/sandbox.js
+++ sqlConnect-Library/sandbox.js
@@ -4,10 +4,10 @@ const mysql = require("mysql2");
 // Create a MySQL connection
 const connection = mysql.createConnection({
   host: "localhost",
-  user: "",
+  user: "root",
   password: "", // Replace with your MySQL root password
   database: "",
-  port: 1122,
+  port: null,
   connectTimeout: 30000, // 30 seconds (adjust as needed)
 });
 
@@ -20,26 +20,33 @@ connection.connect((err) => {
 });
 
 function addBookToDatabase(title, author, year) {
-  const sql = "INSERT INTO books (title, author, book_year) VALUES (?, ?, ?)";
-  const values = [title, author, year];
+  const sql = `INSERT INTO books (title, author, book_year) VALUES (${title}, ${author}, ${year})`;
 
-  connection.query(sql, values, (err, result) => {
+  connection.query(sql, (err, result) => {
     if (err) {
       console.error("Error adding book to database:", err);
     } else {
       console.log(`${title} has been added to the library.`);
     }
+
+    // Close the database connection
+    connection.end();
   });
 }
 
-function addBook(title, author, year) {
-  const book = {
-    title: title,
-    author: author,
-    year: year,
-  };
+function deleteBook() {
+  const title = prompt("Book title: ");
+  const sql = `DELETE FROM books WHERE title = '${title}'`;
+  connection.query(sql, values, (err, result) => {
+    if (err) {
+      console.error("Error deleting book from database:", err);
+    } else {
+      console.log(`${title} has been deleted from the library.`);
+    }
 
-  addBookToDatabase(title, author, year);
+    // Close the database connection
+    connection.end();
+  });
 }
 
 function displayBooks() {
@@ -47,37 +54,53 @@ function displayBooks() {
   console.log("--> List of books in the library");
   library.forEach((book) => {
     console.log(`${book.title} by ${book.author} (${book.year})`);
+    // Close the database connection
+    connection.end();
   });
 }
 
 function searchBook(title) {
   console.log("--------------------------------------");
-  let bookFound = false;
 
-  library.forEach((book) => {
-    if (book.title.toLowerCase() === title.toLowerCase()) {
-      console.log(`Book Found: ${book.title} by ${book.author} (${book.year})`);
-      bookFound = true;
+  const lowerCaseTitle = title.toLowerCase();
+  const sql = `SELECT * FROM books WHERE LOWER(title) = LOWER('${lowerCaseTitle}')`;
+
+  // Execute the query
+  connection.query(sql, (error, results) => {
+    if (error) {
+      throw error;
     }
-  });
 
-  if (!bookFound) {
-    console.log(`Book not found: ${title}`);
-  }
+    // Check if any books were found
+    if (results.length > 0) {
+      results.forEach((book) => {
+        console.log(
+          `Book Found: ${book.title} by ${book.author} (${book.year})`
+        );
+      });
+    } else {
+      console.log(`Book not found: ${title}`);
+    }
+
+    // Close the database connection
+    connection.end();
+  });
 }
 
 const menu = () => {
   while (true) {
-    console.log(`<---- ************************************ ---->`);
+    console.log(`\n<---- ************************************ ---->\n`);
     console.log("<---- Welcome to library management system ---->");
     console.log(`\n<---- select the option ---->\n`);
     console.log("1. Add Book");
     console.log("2. Display Books");
     console.log("3. Search Book");
+    console.log("4. Delete Book");
 
-    const choice = prompt("Enter your choice (1, 2, or 3): ");
+    const choice = prompt("Enter your choice (1, 2, or 3..): ");
 
     if (choice == -1) {
+      connection.end();
       break;
     }
 
@@ -91,6 +114,9 @@ const menu = () => {
       case "3":
         takeSearchBook();
         break;
+      case "4":
+        deleteBook();
+        break;
       default:
         console.log("Invalid choice. Please enter 1, 2, or 3.");
     }
@@ -99,11 +125,11 @@ const menu = () => {
 
 const takeBook = () => {
   console.log(`\n<--- Add book menu --->\n`);
-  const name = prompt("Book name: ");
+  const title = prompt("Book title: ");
   const author = prompt("Author: ");
   const year = prompt("Year: ");
 
-  addBook(name, author, year);
+  addBookToDatabase(title, author, year);
 };
 
 const takeSearchBook = () => {
