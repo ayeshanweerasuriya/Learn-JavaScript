0606cad12f8b905b069a19069beda2a9a81a0465
ayeshanweerasuriya <ayeshanweerasuriya@gmail.com>
2023-12-30 20:31:00 +0530

    addBook implement

--- createSqlSystem/sandbox.js
+++ createSqlSystem/sandbox.js
@@ -1,14 +1,44 @@
 const prompt = require("prompt-sync")();
 
+const library = [];
+
 const addBook = () => {
   console.log(`\n<-----Add-Book-Menu----->\n`);
   const title = prompt("Book Title: ");
   const author = prompt("Book Author: ");
-  const book = prompt("Book Year: ");
+
+  let year;
+
+  while (true) {
+    const bookYear = prompt("Book Year: ");
+    if (isNaN(bookYear)) {
+      console.log("Please enter valid year.");
+    } else {
+      year = bookYear;
+      break;
+    }
+  }
+
+  const book = {
+    title: title,
+    author: author,
+    year: year,
+  };
+
+  library.push(book);
+
+  console.log(`\n${title} added to the library.\n`);
+
+  setTimeout(() => {
+    menu();
+  }, 3000);
 };
 
 const menu = () => {
   while (true) {
+    console.clear();
+    console.log("--------------------------------------------------");
+    console.log("-----Welcome-to-the-Library-Mangement-System----->");
     console.log("1. Add book");
     console.log("2. Delete book");
     console.log("3. Update book");
@@ -32,6 +62,10 @@ const menu = () => {
         break;
       case "5":
         return;
+      default:
+        console.log("Invalid choice. Please enter a valid option.");
+        console.log("Press Enter to continue...");
+        break;
     }
   }
 };
