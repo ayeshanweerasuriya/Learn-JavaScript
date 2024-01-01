66101586de138d83bbfde7fadde17e42f52c0fa7
Ayeshan Weerasuriya <98686381+ayeshanweerasuriya@users.noreply.github.com>
2024-01-01 22:21:44 +0530

    Update script.js
    pattern

--- starPattern/script.js
+++ starPattern/script.js
@@ -1,3 +1,10 @@
 const prompt = require("prompt-sync")();
 
 const num = prompt("Number of levels for the Christmas tree: ");
+
+for (let i = 0; i < num; i++) {
+  for (let j = 0; i < j; j++) {
+    console.log("*");
+  }
+  console.log(" ");
+}
