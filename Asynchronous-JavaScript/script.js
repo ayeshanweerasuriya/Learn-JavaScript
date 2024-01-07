b93c2b011d64f2f8173a7605e9852ead1f822f91
ayeshanweerasuriya <ayeshanweerasuriya@gmail.com>
2024-01-07 13:06:09 +0530

    Fetch api data

--- Asynchronous-JavaScript/script.js
+++ Asynchronous-JavaScript/script.js
@@ -1,3 +1,14 @@
-const request = new XMLHttpRequest(); // create request object
+const request = new XMLHttpRequest(); // (01. create request object
 
-request.open("Get", "https://jsonplaceholder.typicode.com/todos/");
+// (03. tracking the progress of the request
+request.addEventListener("readystatechange", () => {
+  console.log(request, request.readyState); // readyState means request state
+
+  // if it is in 4th step (Done) get the data
+  if (request.readyState === 4) {
+    console.log(request.responseText);
+  }
+});
+
+request.open("GET", "https://jsonplaceholder.typicode.com/todos/"); // (02. setting up type of request and request location
+request.send(); // (03. send request
