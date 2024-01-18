e9f4e9a4ff11d7238360737ded34aea481f9d3b0
ayeshanweerasuriya <ayeshanweerasuriya@gmail.com>
2024-01-18 22:09:11 +0530

    init

--- Rock-Paper-Scissor (M)/script.js
+++ Rock-Paper-Scissor (M)/script.js
@@ -6,8 +6,22 @@ const randomNumber = Math.floor(Math.random() * 3);
 const computerChoice = options[randomNumber];
 
 console.log("Welcome to Rock Paper Scissor Game ---->");
-console.log(`\n 1. Rock \n 2. Paper \n 3. Scissor \n`);
-const userChoice = parseInt(prompt("User Choice: "));
+
+let userChoice = 0;
+
+while (true) {
+  console.log(`\n 1. Rock \n 2. Paper \n 3. Scissor \n 4. Exit`);
+  userChoice = parseInt(prompt("User Choice: "));
+
+  if (userChoice >= 1 && userChoice <= 3) {
+    break;
+  } else if (userChoice === 4) {
+    console.log("Exiting the game.");
+    process.exit();
+  } else {
+    console.log("Invalid choice. Please choose a number between 1 and 4.");
+  }
+}
 
 const userResult = options[userChoice - 1];
 
