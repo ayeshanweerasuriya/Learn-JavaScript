2b18d782ee0ffd622adf018e43c637a39972e58d
ayeshanweerasuriya <ayeshanweerasuriya@gmail.com>
2024-05-22 22:17:38 +0530

    init api call

--- LReact/script.js
+++ LReact/script.js
@@ -1,40 +1,13 @@
-const prompt = require("prompt-sync")();
+const { versatile, nsfw } = require("./tags");
+const { apiCall } = require("./api");
 
-// const name = prompt("Type your name: ");
-// const age = prompt("Type your age: ");
+// console.log(versatile[0]);
+// console.log(nsfw[0]);
 
-const isTalk = () => {
-  console.log(`My name is ${person.name}`);
-  console.log(`I am ${person.age} years old`);
-};
-
-// const person = {
-//   name,
-//   age,
-//   func: isTalk,
-// };
-
-// person.func();
-
-const names = ["Ayeshan", "Melani", "James", "Cobolt"];
-
-// const result = names.map((element) => {
-//   return element + "1";
-// });
-
-// console.log(result);
-
-const names2 = [...names, "hutto"];
-
-// names2.forEach((element) => {
-//   console.log(element + "1");
-// });
-
-// names.filter();
-// names.reduce();
-
-const filter = names.filter((element) => {
-  return element.length != 6;
-});
-
-console.log(filter);
+apiCall()
+  .then((data) => {
+    console.log(data.images[0].url);
+  })
+  .catch((error) => {
+    console.log("Failed to fetch data: ", error.message);
+  });
