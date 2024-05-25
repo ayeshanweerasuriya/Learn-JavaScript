7ff45af74ef7d982ce16f9bdc10d7fb7c4097490
ayeshanweerasuriya <ayeshanweerasuriya@gmail.com>
2024-05-26 01:53:39 +0530

    spread operator

--- LReact/main.js
+++ LReact/main.js
@@ -1,3 +1,25 @@
-const prompt = require("prompt-sync")();
+// const prompt = require("prompt-sync")();
 
 // const
+
+function addressMaker(address) {
+  const { city, state } = address;
+
+  const newAddress = {
+    city,
+    state,
+    country: "Unites States",
+  };
+
+  console.log(city, state, newAddress.country);
+}
+
+addressMaker({ city: "Austin", state: "Texas" });
+
+function add(...nums) {
+  const numArray = nums;
+
+  console.log(numArray);
+}
+
+add(1, 2, 3, 4, 5);
