dbd82917bebe359c8a489c69449503160052a459
ayeshanweerasuriya <ayeshanweerasuriya@gmail.com>
2024-06-26 02:39:44 +0530

    dom manipulation

--- 8hrs/script.js
+++ 8hrs/script.js
@@ -1,3 +1,40 @@
-document.querySelector(".submit").addEventListener("click", () => {
-  console.log(document.querySelector(".name").value);
-});
+const body = document.body;
+
+// const div = document.createElement("div");
+
+// const strong = document.createElement("strong");
+
+// strong.innerHTML = "Hello World";
+
+// div.append(strong);
+
+// body.append(div);
+
+// const h1 = document.getElementById("he");
+// h1.remove();
+
+// console.log(h1.getAttribute("id"));
+// console.log(h1.title);
+// console.log(h1.id);
+
+// h1.setAttribute("title", "joshua");
+
+// const div = document.createElement("div");
+// body.append(div);
+
+// div.setAttribute("id", "this-div");
+// div.setAttribute("title", "James");
+// div.textContent = "Hello James";
+
+// div.removeAttribute("title");
+// div.removeAttribute("id");
+// div.setAttribute("class", "this-div");
+
+const h2 = document.querySelector("#h2-c");
+h2.textContent = "Jason";
+// h2.classList.add("josh-ham");
+// h2.classList.remove("josh");
+
+h2.classList.toggle("jason"); // available ? remove : add
+
+h2.style.display = "block";
